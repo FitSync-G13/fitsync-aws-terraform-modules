80c81b64b13047f554048075e3a0ea8daf44c425
wickrasofte <wickrasoft@gmail.com>
2025-12-10 12:17:47 +0100

    Non Prod Terraform Modules updated

---
+++ live/non-prod/spoke-obs/outputs.tf
@@ -0,0 +1,24 @@
+output "k3s_api_dns" {
+  description = "DNS name of the K3s API load balancer"
+  value       = module.spoke.k3s_api_dns
+}
+
+output "master_private_ips" {
+  description = "Private IP addresses of K3s master nodes"
+  value       = module.spoke.master_private_ips
+}
+
+output "worker_private_ips" {
+  description = "Private IP addresses of K3s worker nodes"
+  value       = module.spoke.worker_private_ips
+}
+
+output "db_private_ips" {
+  description = "Private IP addresses of database nodes"
+  value       = module.spoke.db_private_ips
+}
+
+output "public_alb_dns" {
+  description = "Public NLB DNS for internet access"
+  value       = module.spoke.public_alb_dns
+}
---
+++ live/non-prod/spoke-obs/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
